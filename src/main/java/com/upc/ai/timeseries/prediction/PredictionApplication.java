666fb81b74dc72657496225c69224abd741df2ef
@ronmiand <ronmiand@gmail.com>
2022-07-11 01:11:59 -0500

    modified main classes

--- src/main/java/com/upc/ai/timeseries/prediction/PredictionApplication.java
+++ src/main/java/com/upc/ai/timeseries/prediction/PredictionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PredictionApplication {
-
+ //init
 	public static void main(String[] args) {
 		SpringApplication.run(PredictionApplication.class, args);
 	}
