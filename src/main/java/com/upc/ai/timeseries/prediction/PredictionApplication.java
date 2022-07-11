315dc4c173b1e6c8ae7dfef7e7c0a478c99b8345
@ronmiand <ronmiand@gmail.com>
2022-07-10 22:23:51 -0500

    initial load

---
+++ src/main/java/com/upc/ai/timeseries/prediction/PredictionApplication.java
@@ -0,0 +1,13 @@
+package com.upc.ai.timeseries.prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/ai/timeseries/prediction/PredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.ai.timeseries.prediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
